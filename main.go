128a913758ae614ca0ae9e73ca58e339238dec1a
Nursil Amirzyanov <mrnursil@gmail.com>
2019-05-15 14:28:47 +0300

    Add a simple server that get data from an agent

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/logs", logsHandler)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func logsHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+	w.Write([]byte("succes"))
 }
